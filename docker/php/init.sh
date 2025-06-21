786e691d466041ddaa0b468bd83dad95fbe6ac8e
Adi Wirawan <dirumahrafif@gmail.com>
2025-06-21 17:37:06 +0700

    first commit

---
+++ docker/php/init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+echo "Hapus default folder html yang sudah ada"
+rm -rf /var/www/html
+
+if [ ! -f /var/www/composer.json ]; then
+  echo "Laravel belum ada, instalasi dimulai..."
+  cd /var/www
+  composer create-project laravel/laravel="^12.0" .
+else
+  echo "Laravel sudah terpasang, skip instalasi."
+fi
+
+echo "Mengatur hak akses direktori storage dan bootstrap/cache..."
+# Buat folder dan file log jika belum ada
+mkdir -p /var/www/storage/logs
+touch /var/www/storage/logs/laravel.log
+# Baru set permission
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+
+cd /var/www
+
+echo "Menjalankan optimisasi Laravel..."
+composer validate --strict
+composer install --optimize-autoloader --no-dev
+php artisan config:clear
+php artisan cache:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Jalankan Apache agar container tetap aktif
+exec apache2-foreground
+
