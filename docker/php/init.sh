336e799013e7cb013dbe4108fcc2ec7dd052fc4d
Adi Wirawan <dirumahrafif@gmail.com>
2025-06-22 17:12:19 +0700

    cek environment: hilangkan cache jika development

--- docker/php/init.sh
+++ docker/php/init.sh
@@ -8,7 +8,7 @@ rm -rf /var/www/html
 if [ ! -f /var/www/composer.json ]; then
   echo "Laravel belum ada, instalasi dimulai..."
   cd /var/www
-  composer create-project laravel/laravel="^12.0" .
+  composer create-project laravel/laravel="12.*" .
 else
   echo "Laravel sudah terpasang, skip instalasi."
 fi
@@ -22,15 +22,31 @@ chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
 cd /var/www
+# Cek mode dari ENV atau default ke 'development'
+APP_ENV=${APP_ENV:-development}
 
 echo "Menjalankan optimisasi Laravel..."
 composer validate --strict
 composer install --optimize-autoloader --no-dev
-php artisan config:clear
-php artisan cache:clear
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+
+# Jalankan perintah artisan sesuai environment
+if [ "$APP_ENV" = "production" ]; then
+    echo "Mode production: menjalankan caching..."
+    php artisan config:clear
+    php artisan cache:clear
+    php artisan view:clear
+    php artisan route:clear
+
+    php artisan config:cache
+    php artisan route:cache
+    php artisan view:cache
+else
+    echo "Mode development: membersihkan cache..."
+    php artisan config:clear
+    php artisan cache:clear
+    php artisan view:clear
+    php artisan route:clear
+fi
 
 # Jalankan Apache agar container tetap aktif
 exec apache2-foreground
