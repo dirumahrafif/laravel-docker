f43f672556cf29ae177ff12814e0e7670bcaaf1b
Adi Wirawan <dirumahrafif@gmail.com>
2025-06-28 04:20:08 +0700

    base

---
+++ laravel_app/app/Http/Controllers/HalloController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HalloController extends Controller
+{
+    //
+    public function index()
+    {
+        echo "<h1>Halo dari Laravel Docker</h1>";
+    }
+}
